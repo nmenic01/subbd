7455680f144310c84f7b85f0ce8f95d044d42c3c
Nicolas <nicolas@teampeanut.com>
2024-12-16 16:16:58 +0200

    fix unescaped entity

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -4,7 +4,7 @@ import { ArrowLeftIcon } from "lucide-react";
 export default function NotFound() {
     return (
         <main className="min-h-screen bg-background flex flex-col items-center justify-center gap-12">
-            <h1 className="lg:text-6xl text-4xl font-bold text-white/80 text-center items-center justify-center">There's nothing here. 😇</h1>
+            <h1 className="lg:text-6xl text-4xl font-bold text-white/80 text-center items-center justify-center">There&apos;s nothing here. 😇</h1>
             <Link href="/" className="text-white/80 lg:text-4xl text-2xl font-bold hover:text-white transition-colors flex items-center gap-2">
                 <ArrowLeftIcon className="lg:w-12 lg:h-12 w-8 h-8" />
                 Go back to home
